df771e8e835d152e67cfde291093fb5c880060be
wilhellmina <x@wakiga.nl>
2022-01-15 10:35:32 +0900

    impl operator+, exceptions

--- ex-2-main.cpp
+++ ex-2-main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <sstream>
 #include <string>
- 
+#include <stdexcept>
+
+#define mcxi_max 9999
+#define mcxi_min 0
+#define mcxi_maximum_unit 1000
+
 namespace cpp2 {
     /* --------------------------------------------------------------------- */
     /*
@@ -28,16 +33,35 @@ namespace cpp2 {
         /* ----------------------------------------------------------------- */
         mcxi(const std::string& s) : value_(0) {
         	int num = 0;
+        	uint16_t mcxi_unit_order = mcxi_maximum_unit;
+
         	for(auto pos = s.begin(); pos != s.end(); pos++){
+        		if(*pos <= '1'){
+        			throw std::invalid_argument("invalid args, 1");
+        		}
         		if(*pos >= '2' && *pos <= '9'){
+        			//2
+        			if(num != 0){
+        				throw std::invalid_argument("invalid args, 2");
+        			}
         			num = *pos - '0';
-        		}else{
+        		}
+        		//1
+        		else{
+        			//1
+        			if(unit(*pos) == -1){
+        				throw std::invalid_argument("invalid args, 1");
+        			}
+        			//fsm in order of m -> c -> x -> i
+        			if(mcxi_unit_order <= unit(*pos)){
+        				throw std::invalid_argument("invalid args, 3");
+        			}
         			int u = unit(*pos);
+
         			value_ += std::max(num,1) * u; //std::max
         			num = 0;
         		}
         	}
-
         }
 
 
@@ -49,7 +73,13 @@ namespace cpp2 {
          */
         /* ----------------------------------------------------------------- */
         mcxi operator+(const mcxi& rhs) {
-            return rhs;
+        	mcxi ret("");
+        	ret.value_ = this->value_ + rhs.value_;
+        	//error handle
+        	if(ret.value_ > mcxi_max){
+        		throw std::overflow_error("overflow occurred");
+        	}
+            return ret;
         }
 
         /* ----------------------------------------------------------------- */
@@ -60,7 +90,9 @@ namespace cpp2 {
          */
         /* ----------------------------------------------------------------- */
         std::string to_string() const {
-        	//error handle if val_ < 9999
+        	if(value_ > mcxi_max || value_ < mcxi_min){
+        		throw std::range_error("");
+        	}
 
         	uint16_t temp;
         	std::stringstream ret;
@@ -103,8 +135,7 @@ namespace cpp2 {
 				ret << temp;
 				ret << 'i';
 			}
-
-        	  return ret.str();
+			return ret.str();
         }
 
     private:
@@ -114,7 +145,7 @@ namespace cpp2 {
 				case 'c' : return 100;
 				case 'x' : return 10;
 				case 'i' : return 1;
-				default : return 0;
+				default : return -1;
         	}
         }
 
@@ -123,61 +154,63 @@ namespace cpp2 {
 }
  
 int main(){
-    cpp2::mcxi a0("m2c4i");
-    cpp2::mcxi b0("x9i");
+	try{
+		cpp2::mcxi a0("xi");
+		cpp2::mcxi b0("x9i");
 
-    std::cout << a0.to_string() << std::endl;;
+		std::cout << a0.to_string() << std::endl;;
 
-#if 0
-    cpp2::mcxi result0 = a0 + b0;
-    std::cout << "3x" <<" " << result0.to_string() << std::endl;
+		cpp2::mcxi result0 = a0 + b0;
+		std::cout << "3x" <<" " << result0.to_string() << std::endl;
 
+		cpp2::mcxi a1("i");
+		cpp2::mcxi b1("9i");
+		cpp2::mcxi result1 = a1 + b1;
+		std::cout << "x" << " " << result1.to_string() << std::endl;
 
-    cpp2::mcxi a1("i");
-    cpp2::mcxi b1("9i");
-    cpp2::mcxi result1 = a1 + b1;
-    std::cout << "x" << " " << result1.to_string() << std::endl;
+		cpp2::mcxi a2("c2x2i");
+		cpp2::mcxi b2("4c8x8i");
+		cpp2::mcxi result2 = a2 + b2;
+		std::cout << "6cx" << " " << result2.to_string() << std::endl;
 
-    cpp2::mcxi a2("c2x2i");
-    cpp2::mcxi b2("4c8x8i");
-    cpp2::mcxi result2 = a2 + b2;
-    std::cout << "6cx" << " " << result2.to_string() << std::endl;
-    
-    cpp2::mcxi a3("m2ci");
-    cpp2::mcxi b3("4m7c9x8i");
-    cpp2::mcxi result3 = a3 + b3;
-    std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
+		cpp2::mcxi a3("m2ci");
+		cpp2::mcxi b3("4m7c9x8i");
+		cpp2::mcxi result3 = a3 + b3;
+		std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
 
-    cpp2::mcxi a4("9c9x9i");
-    cpp2::mcxi b4("i");
-    cpp2::mcxi result4 = a4 + b4;
-    std::cout << "m" << " " << result4.to_string() << std::endl;
+		cpp2::mcxi a4("9c9x9i");
+		cpp2::mcxi b4("i");
+		cpp2::mcxi result4 = a4 + b4;
+		std::cout << "m" << " " << result4.to_string() << std::endl;
 
-    cpp2::mcxi a5("i");
-    cpp2::mcxi b5("9m9c9x8i");
-    cpp2::mcxi result5 = a5 + b5;
-    std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
+		cpp2::mcxi a5("i");
+		cpp2::mcxi b5("9m9c9x8i");
+		cpp2::mcxi result5 = a5 + b5;
+		std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
 
-    cpp2::mcxi a6("m");
-    cpp2::mcxi b6("i");
-    cpp2::mcxi result6 = a6 + b6;
-    std::cout << "mi" << " " << result6.to_string() << std::endl;
+		cpp2::mcxi a6("m");
+		cpp2::mcxi b6("i");
+		cpp2::mcxi result6 = a6 + b6;
+		std::cout << "mi" << " " << result6.to_string() << std::endl;
 
-    cpp2::mcxi a7("i");
-    cpp2::mcxi b7("m");
-    cpp2::mcxi result7 = a7 + b7;
-    std::cout << "mi" << " " << result7.to_string() << std::endl;
+		cpp2::mcxi a7("i");
+		cpp2::mcxi b7("m");
+		cpp2::mcxi result7 = a7 + b7;
+		std::cout << "mi" << " " << result7.to_string() << std::endl;
 
-    cpp2::mcxi a8("m9i");
-    cpp2::mcxi b8("i");
-    cpp2::mcxi result8 = a8 + b8;
-    std::cout << "mx" << " " << result8.to_string() << std::endl;
+		cpp2::mcxi a8("m9i");
+		cpp2::mcxi b8("i");
+		cpp2::mcxi result8 = a8 + b8;
+		std::cout << "mx" << " " << result8.to_string() << std::endl;
 
-    cpp2::mcxi a9("9m8c7xi");
-    cpp2::mcxi b9("c2x8i");
-    cpp2::mcxi result9 = a9 + b9;
-    std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
-#endif
+		cpp2::mcxi a9("9m8c7xi");
+		cpp2::mcxi b9("c2x8i");
+		cpp2::mcxi result9 = a9 + b9;
+		std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
+	}
+	catch(...){
+		std::cout << "some exception" << std::endl;
+	}
 
     return 0;
 }
